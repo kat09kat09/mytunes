2edebf9d66ceae9e87969820bca34ec41dbc4c37
kat09kat09 <kat09kat09@gmail.com>
2015-12-09 22:47:04 -0500

    passed test for song model

--- client/models/SongModel.js
+++ client/models/SongModel.js
@@ -1,9 +1,24 @@
+
 // SongModel.js - Defines a backbone model class for songs.
 var SongModel = Backbone.Model.extend({
 
   play: function(){
     // Triggering an event here will also trigger the event on the collection
     this.trigger('play', this);
+  },
+
+  enqueue: function(){
+// Triggering an event here will also trigger the event on the collection
+    this.trigger('enqueue', this);
+  },
+  dequeue: function(){
+// Triggering an event here will also trigger the event on the collection
+    this.trigger('dequeue', this);
+  },
+  ended: function(){
+// Triggering an event here will also trigger the event on the collection
+    this.trigger('ended', this);
   }
 
 });
+
